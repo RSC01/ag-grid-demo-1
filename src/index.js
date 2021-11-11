9cf192cf4de3fe326e8949c5fe08aab07d4608ca
GhusarParth <parthghusar@bigscal.com>
2021-11-11 12:34:12 +0530

    updated  index.js file

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* resolve testing comment for index  js */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
